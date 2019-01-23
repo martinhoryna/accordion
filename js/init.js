20db16f95daf4272c60623a3d6a0a6411b8e403d
Martin Horyna <martinhoryna1@gmail.com>
2019-01-23 15:32:09 +0100

    accordion DONE MOTHERFRS

--- js/init.js
+++ js/init.js
@@ -2,7 +2,23 @@ $(document).ready(function () {
     $(".box").hide();
     $("h4").click(function(){
         var index = $(this).index("h4");
-        $(".box:eq("+index+")").slideToggle();
-        $(this).addClass("active");
+        $(".box:eq("+index+")").stop().slideToggle();
+        $(this).toggleClass("active")
+        if ($(this).hasClass("active")){
+            $(this).find("i").removeClass("far fa-arrow-alt-circle-down");
+            $(this).find("i").addClass("far fa-arrow-alt-circle-up");
+        }else{
+            $(this).find("i").removeClass("far fa-arrow-alt-circle-up");
+            $(this).find("i").addClass("far fa-arrow-alt-circle-down");
+
+
+        }
     })
 });
+
+
+
+/*$(document).ready(function () {
+    $(".box").hide();
+    $("h4").click(f)
+})*/
