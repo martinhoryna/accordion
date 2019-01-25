e6aba88dfb475c3663daa862139bf355ad0c479f
Martin Horyna <martinhoryna1@gmail.com>
2019-01-25 11:56:42 +0100

    accordion next()

--- js/init.js
+++ js/init.js
@@ -1,4 +1,7 @@
-$(document).ready(function () {
+
+// eq acc
+
+/*$(document).ready(function () {
     $(".box").hide();
     $("h4").click(function(){
         var index = $(this).index("h4");
@@ -15,10 +18,25 @@ $(document).ready(function () {
         }
     })
 });
+*/
 
+// next() acc**************************************************************************************--
 
+$(document).ready(function () {
+    $(".box").each(function () {
+        $(this).find("h4").click(function () {
+            $(this).next().stop().slideToggle();
+            $(this).toggleClass("active")
 
-/*$(document).ready(function () {
-    $(".box").hide();
-    $("h4").click(f)
-})*/
+            if ($(this).hasClass("active")){
+                $(this).find("i").removeClass("fas fa-angle-down")
+                $(this).find("i").addClass("fas fa-angle-up")
+            }else{
+
+                $(this).find("i").removeClass("fas fa-angle-up")
+                $(this).find("i").addClass("fas fa-angle-down")
+
+            }
+        })
+    })
+})
